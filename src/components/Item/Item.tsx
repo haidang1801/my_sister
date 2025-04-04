54bad513aba2e01a3708b608590505b34c5acf25
hai.dh <hai0973839203@gmail.com>
2025-04-04 10:09:43 +0700

    update send image

--- src/components/Item/Item.tsx
+++ src/components/Item/Item.tsx
@@ -1,11 +1,26 @@
 import './Item.css';
 const Item = (props: any) => {
+
+  const sendImage = () => {
+    var imageLink = `https://hoang-ngan-wedding.vercel.app/${props.image}`;
+    var appId = "2493083654376489";
+    var redirectUri = `https://hoang-ngan-wedding.vercel.app/${props.image}`;
+
+    var messengerUrl = `https://www.facebook.com/dialog/send?
+        app_id=${appId}
+        &link=${encodeURIComponent(imageLink)}
+        &redirect_uri=${encodeURIComponent(redirectUri)}`;
+
+        window.open(messengerUrl, "_blank");
+        
+  }
+
   return (
     <div className='item'>
-      <img src={props.image} alt='' ></img>
+      <img src={props.image} alt={props.name} ></img>
       <div className="item-properties">
         <p className='item-text'>{props.name}</p>
-        <button className='item-button'>Gửi ảnh</button>
+        <button className='item-button' onClick={sendImage}>Gửi ảnh</button>
       </div>
     </div>
   )
